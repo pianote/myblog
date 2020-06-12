3ca6304181a1d0a753df1c9f93302963ef5ac36a
Duc Vo <pianote@gmail.com>
2020-06-12 13:14:09 -0700

    change description

--- users/permissions.py
+++ users/permissions.py
@@ -11,5 +11,5 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
 
-        # Write permissions are only allowed to the owner of the snippet.
+        # Write permissions are only allowed to the owner of the profile.
         return obj.user == request.user
