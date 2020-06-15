ced31fc2b959f86d7296829eedf6cfd61e990589
Duc Vo <pianote@gmail.com>
2020-06-15 15:17:29 -0700

    add Reply model

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,34 +1,75 @@
 from rest_framework import serializers
-from .models import Post, Comment
+from .models import Post, Comment, Reply
+
+class ReplyListSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.name')
+    # comment = serializers.ReadOnlyField(source='comment.comment_content')
+    class Meta:
+        model = Reply
+        fields = ['author','reply_content']
+
+class CommentListSerializer(serializers.ModelSerializer):
+    post = serializers.HyperlinkedRelatedField(view_name="blog:post-detail", read_only=True)
+    url = serializers.HyperlinkedIdentityField(view_name="blog:comment-detail")
+    reply_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Comment
+        fields = ['url', 'post', 'reply_count']
+    
+    def get_reply_count(self, obj):
+        count = Reply.objects.filter(comment=obj).count()
+        return count
+
+
+class CommentDetailSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.name')
+    post = serializers.HyperlinkedRelatedField(view_name="blog:post-detail", read_only=True)
+    replies = ReplyListSerializer(many=True, read_only=True)
+    class Meta:
+        model = Comment
+        fields = ['post','author','comment_content', 'replies']
+    
+    def get_replies(self, obj):
+        queryset = Reply.objects.filter(comment=obj)
+        replies = ReplyListSerializer(queryset, many=True).data
+        return replies
 
 class PostListSerializer(serializers.HyperlinkedModelSerializer):
-    author = serializers.ReadOnlyField(source='author.name')
     url = serializers.HyperlinkedIdentityField(view_name="blog:post-detail")
-    highlight = serializers.HyperlinkedIdentityField(view_name='blog:post-highlight', format='html')
-    comments = serializers.HyperlinkedRelatedField(many=True, view_name='blog:comment-detail',read_only=True)
+    comment_count = serializers.SerializerMethodField() #serializers field type is important to use getter method
 
     class Meta:
         model = Post
-        fields = ['author','title','content','date_posted','url','highlight','comments']
+        fields = ['url','comment_count']
+    
+    def get_comment_count(self, obj):
+        count = Comment.objects.filter(post=obj).count()
+        return count
 
 class PostDetailSerializer(serializers.HyperlinkedModelSerializer):
     author = serializers.ReadOnlyField(source='author.name')
-    url = serializers.HyperlinkedIdentityField(view_name="blog:post-detail")
+    # url = serializers.HyperlinkedIdentityField(view_name="blog:post-detail")
     highlight = serializers.HyperlinkedIdentityField(view_name='blog:post-highlight', format='html')
-    comments = serializers.HyperlinkedRelatedField(many=True, view_name='blog:comment-detail',read_only=True)
+    # comments = serializers.HyperlinkedRelatedField(many=True, view_name='blog:comment-detail',read_only=True)
+    comments = CommentListSerializer(many=True, read_only=True)
+    # comments = CommentDetailSerializer(many=True)
 
     class Meta:
         model = Post
-        fields = ['author','title','date_posted','url','highlight','content','is_published','comments',
+        fields = ['title','author','content', 'highlight','date_posted','is_published','comments',
                 'photo_main','photo_1','photo_2','photo_3']
 
-class CommentSerializer(serializers.ModelSerializer):
-    # comment_author = serializers.ReadOnlyField(source='comment_author.name')
-    # post = serializers.ReadOnlyField(source='post.title')
-    # url = serializers.HyperlinkedIdentityField(view_name="blog:comment-detail")
-    class Meta:
-        model = Comment
-        fields = ['post','comment_author','comment_content']
+    def get_comments(self, obj):
+        queryset = Comment.objects.filter(post=obj)
+        comments = CommentListSerializer(queryset, many=True).data
+        return comments
+    
+    # def get_comments(self, obj):
+    #     queryset = Comment.objects.filter(post=obj)
+    #     comments_detail = CommentDetailSerializer(queryset, many=True).data
+    #     return comments_detail
 
+        
 class EmptySerializer(serializers.Serializer):
     pass
--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import PostViewSet, CommentViewSet
+from .views import PostViewSet, CommentViewSet, ReplyViewSet
 app_name = 'blog'
 
 router = routers.DefaultRouter(trailing_slash=True)
-router.register('posts', PostViewSet) #post-detail, post-list ...
+router.register('posts', PostViewSet) 
 router.register('comments', CommentViewSet)
+router.register('replies', ReplyViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
